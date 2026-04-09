cd3df10ae2fe1b0f0fe086f1ccbc575b5fbc0500
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-04-09 21:31:04 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// api/server.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add a rewrite to handle /api/* routes
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
+server.use(router);
+
+module.exports = server;
